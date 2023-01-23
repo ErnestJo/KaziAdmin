06afb959dcf1712aef3c06b22748b28b3b05371d
ErnestJo <smartlabstech@yahoo.com>
2023-01-23 23:17:36 +0300

    Assest

---
+++ components/footer-component.vue
@@ -0,0 +1,9 @@
+<template>
+    <v-container>
+      <v-footer app>
+        <v-spacer />
+        <span>CopyRight &copy; {{ new Date().getFullYear() }} Kazi Admin</span>
+      </v-footer>
+    </v-container>
+  </template>
+  
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,63 +3,131 @@
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
+      :clipped="false"
+      permanent
+      color="primary"
+      dark
       app
+      height="100%"
+      style="max-height: 97.6%;"
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+        <v-list-item v-if="miniVariant" class="px-2">
+          <v-list-item-avatar color="whitish">
+            <v-img :src="miniUrl" class="vuetify-logo" />
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item v-else link>
+          <v-list-item-avatar color="whitish">
+            <v-img :src="miniUrl" class="vuetify-logo" />
+          </v-list-item-avatar>
           <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+            <v-list-item-title class="text-h6">
+              NgaziTech Platform
+            </v-list-item-title>
+            <v-list-item-subtitle>Transactions management</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list>
+
+      <v-divider />
+      <v-list-item
+        v-for="child in menus"
+        :key="child.title"
+        :to="child.to"
+        color="success"
+        class="pl-4"
+      >
+        <v-list-item-icon>
+          <v-icon>{{ 'mdi-'+ child.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            <span class="font-weight-light">{{ child.title }}</span>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <!--<v-list dense>
+        <v-list-group
+          v-for="item in menuitems"
+          :key="item.title"
+          v-model="item.active"
+          :prepend-icon="`mdi-${item.icon}`"
+          active-class="white--text"
+          append-icon="mdi-menu-down"
+          no-action
+        >
+          <template #activator>
+            <v-list-item-content>
+              <v-list-item-title class="font-weight-bold">
+                {{ item.title }}
+              </v-list-item-title>
+            </v-list-item-content>
+          </template>
+
+          <v-list-item
+            v-for="child in item.menus"
+            :key="child.title"
+            color="primary"
+            class="pl-4"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ 'mdi-'+ child.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>
+                <span class="font-weight-light">{{ child.title }}</span>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-group>
+      </v-list>-->
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+      color="primary"
+      flat
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!--<v-app-bar-nav-icon @click.stop="drawer = !drawer" />-->
       <v-btn
         icon
+        dark
         @click.stop="miniVariant = !miniVariant"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+        <v-icon>mdi-{{ `menu${miniVariant ? '' : '-open'}` }}</v-icon>
       </v-btn>
       <v-btn
         icon
-        @click.stop="clipped = !clipped"
+        dark
       >
-        <v-icon>mdi-application</v-icon>
+        <v-icon>mdi-selection</v-icon>
       </v-btn>
       <v-btn
         icon
-        @click.stop="fixed = !fixed"
+        dark
       >
-        <v-icon>mdi-minus</v-icon>
+        <v-icon>mdi-home</v-icon>
       </v-btn>
-      <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
+
+      <v-badge
+        :content="notifications"
+        :value="notifications"
+        color="secondary"
+        class="ml-6"
+        overlap
       >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+        <v-icon dark>
+          mdi-bell
+        </v-icon>
+      </v-badge>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <Nuxt />
+      <v-container class="ma-2" height="100%" fluid>
+        <nuxt />
       </v-container>
     </v-main>
     <v-navigation-drawer
@@ -83,36 +151,180 @@
       :absolute="!fixed"
       app
     >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <footer-component />
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import FooterComponent from '~/components/footer-component.vue'
 export default {
   name: 'DefaultLayout',
+  components: {
+    'footer-component': FooterComponent
+  },
   data () {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
+      messages: 8,
+      adv: 7,
+      notifications: 1,
+      menuitems: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
+          title: 'System',
+          icon: 'cog-transfer-outline',
+          menus: [
+            {
+              title: 'Users',
+              icon: 'account-cog'
+            },
+            {
+              title: 'Settings',
+              icon: 'cog'
+            }
+          ]
+        },
+        {
+          title: 'Archive',
+          icon: 'bookshelf',
+          menus: [
+            {
+              title: 'Power Grid',
+              icon: 'sitemap-outline'
+            },
+            {
+              title: 'Meter',
+              icon: 'gauge'
+            },
+            {
+              title: 'Customer',
+              icon: 'account-group'
+            },
+            {
+              title: 'POC',
+              icon: 'map-marker-path'
+            },
+            {
+              title: 'DCU',
+              icon: 'access-point'
+            },
+            {
+              title: 'QES',
+              icon: 'dots-circle'
+            },
+            {
+              title: 'Line Loss Rule',
+              icon: 'source-branch'
+            },
+            {
+              title: 'VEE Work Group',
+              icon: 'widgets'
+            },
+            {
+              title: 'Install Survey',
+              icon: 'magnify'
+            },
+            {
+              title: 'Install Plan',
+              icon: 'crosshairs'
+            },
+            {
+              title: 'Install Order',
+              icon: 'stack-overflow'
+            },
+            {
+              title: 'Help Desk',
+              icon: 'information'
+            }
+          ]
+        },
+        {
+          title: 'Prepaid',
+          icon: 'domain',
+          menus: []
+        },
+        {
+          title: 'Service Request',
+          icon: 'cloud-download',
+          menus: []
+        },
+        {
+          title: 'Analytics',
+          icon: 'chart-line'
+        },
+        {
+          title: 'Report',
+          icon: 'file-key',
+          menus: []
+        },
+        {
+          title: 'Workflow',
+          icon: 'arrow-decision',
+          menus: []
+        }
+      ],
+      menus: [
+        {
+          title: 'Home',
+          icon: 'view-dashboard',
           to: '/'
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          title: 'Organizations',
+          icon: 'account-group',
+          to: '/groups'
+        },
+      //   {
+      //     title: 'Banks',
+      //     icon: 'bank',
+      //     to: '/banks'
+      //   },
+      //  {
+      //     title: 'DCU',
+      //     icon: 'access-point'
+      //   },
+      //   {
+      //     title: 'QES',
+      //     icon: 'dots-circle'
+      //   },
+      //   {
+      //     title: 'Line Loss Rule',
+      //     icon: 'source-branch'
+      //   },
+      //   {
+      //     title: 'VEE Work Group',
+      //     icon: 'widgets'
+      //   },
+      //   {
+      //     title: 'Install Survey',
+      //     icon: 'magnify'
+      //   },
+      //   {
+      //     title: 'Install Plan',
+      //     icon: 'crosshairs'
+      //   },
+      //   {
+      //     title: 'Install Order',
+      //     icon: 'stack-overflow'
+      //   },
+      //   {
+      //     title: 'Help Desk',
+      //     icon: 'information'
+      //   }
+   
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.js',
+      url: 'logo.png',
+      miniUrl: 'logo.png'
     }
+  },
+  created () {
+    localStorage.setItem('msisdn', '255754710521')
   }
 }
 </script>
